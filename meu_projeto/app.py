de5898dca8d52a203c808f4883d1aad28c3dcde3
Pedro Henrique <pedrohenriquesb4002@gmail.com>
2026-03-18 16:16:35 -0300

    ajuste de pastas

--- meu_projeto/app.py
+++ meu_projeto/app.py
@@ -37,5 +37,9 @@ def convert():
     finally:
         if os.path.exists(path): os.remove(path)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # O Render/Railway usa uma porta variável, por isso usamos o 'PORT'
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
